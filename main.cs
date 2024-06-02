fb85aaef1330a406b89572a00ea114f1fd6b99cf
Cannonbarrel <Firenoob1001@gmail.com>
2024-06-02 23:05:46 +0000

    My program for Exercise 2.1.1

--- main.cs
+++ main.cs
@@ -2,6 +2,17 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+
+            const double CentimetersInInch = 2.54;
+
+            Console.Write("Enter a measurement in inches: ");
+            string input = Console.ReadLine();
+
+            double inches = Convert.ToDouble(input);
+
+            double centimeters = inches * CentimetersInInch;
+
+            Console.WriteLine("{0} inches is {1} centimeters.", inches, centimeters);
+
   }
 }
